dd2002e8d1cbafd628c4b9db1e4871b27e37d92d
ming <mwei0210@gmail.com>
2020-03-12 12:13:41 +0800

    return multiple grids locust master ip

--- services/swarmhub/src/swarmhub/tf/tf.go
+++ services/swarmhub/src/swarmhub/tf/tf.go
@@ -33,16 +33,21 @@ func terraformOutput(statePath, key string) (string, error) {
 func GetMasterIP(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Println("Getting master IP")
 	testID := ps.ByName("id")
+	type outputGrid struct {
+		ID   string
+		Name string
+		IP   string
+	}
 	type output struct {
 		Status      string
-		IP          string
+		Grids       []outputGrid
 		Auth        string
 		Description string
 	}
 
 	cookie, err := r.Cookie("Authorization")
 	if err != nil {
-		_output := output{Status: "Failed", IP: "", Auth: "", Description: "Failed to get auth token from cookie."}
+		_output := output{Status: "Failed", Grids: nil, Auth: "", Description: "Failed to get auth token from cookie."}
 		b, _ := json.Marshal(_output)
 		w.Write(b)
 		return
@@ -54,9 +59,9 @@ func GetMasterIP(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	}})
 
 	// step 1. use testID and get grid ID and grid region
-	gridID, gridRegion, err := db.GetGridByTestID(testID)
+	grids, err := db.GetGridsByTestId(testID)
 	if err != nil {
-		_output := output{Status: "Failed", IP: "", Auth: "", Description: err.Error()}
+		_output := output{Status: "Failed", Grids: nil, Auth: "", Description: err.Error()}
 		b, _ := json.Marshal(_output)
 		w.Write(b)
 		return
@@ -67,70 +72,76 @@ func GetMasterIP(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		Credentials: cred,
 	}))
 
-	objName := fmt.Sprintf("%s-%s-PROVISION", gridID, gridRegion)
+	var outputGrids []outputGrid
 
-	input := &s3.GetObjectInput{
-		Bucket: aws.String(os.Getenv("AWS_S3_BUCKET_TFSTATE")),
-		Key:    aws.String(objName),
-	}
+	for _, grid := range grids {
 
-	result, err := svc.GetObject(input)
-	if err != nil {
-		var errorMessage string
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			//case s3.ErrCodeNoSuchKey:
-			//	fmt.Println(s3.ErrCodeNoSuchKey, aerr.Error())
-			default:
-				errorMessage = aerr.Error()
+		objName := fmt.Sprintf("%s-%s-%s-PROVISION", os.Getenv("DEPLOYMENT_NAME"), grid.ID, grid.Region)
+
+		input := &s3.GetObjectInput{
+			Bucket: aws.String(os.Getenv("AWS_S3_BUCKET_TFSTATE")),
+			Key:    aws.String(objName),
+		}
+
+		result, err := svc.GetObject(input)
+		if err != nil {
+			var errorMessage string
+			if aerr, ok := err.(awserr.Error); ok {
+				switch aerr.Code() {
+				//case s3.ErrCodeNoSuchKey:
+				//	fmt.Println(s3.ErrCodeNoSuchKey, aerr.Error())
+				default:
+					errorMessage = aerr.Error()
+				}
+			} else {
+				// Print the error, cast err to awserr.Error to get the Code and
+				// Message from an error.
+				errorMessage = err.Error()
 			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			errorMessage = err.Error()
+			_output := output{Status: "Failed", Grids: nil, Auth: "", Description: errorMessage}
+			b, _ := json.Marshal(_output)
+			w.Write(b)
+			return
 		}
-		_output := output{Status: "Failed", IP: "", Auth: "", Description: errorMessage}
-		b, _ := json.Marshal(_output)
-		w.Write(b)
-		return
-	}
 
-	stateFilePath := os.Getenv("TFSTATE_DIR_PATH") + "/" + objName
-	statefile, err := os.Create(stateFilePath)
-	if err != nil {
-		_output := output{Status: "Failed", IP: "", Auth: "", Description: err.Error()}
-		b, _ := json.Marshal(_output)
-		w.Write(b)
-		return
+		stateFilePath := os.Getenv("TFSTATE_DIR_PATH") + "/" + objName
+		statefile, err := os.Create(stateFilePath)
+		if err != nil {
+			_output := output{Status: "Failed", Grids: nil, Auth: "", Description: err.Error()}
+			b, _ := json.Marshal(_output)
+			w.Write(b)
+			return
 
-	}
-
-	if _, err := io.Copy(statefile, result.Body); err != nil {
-		_output := output{Status: "Failed", IP: "", Auth: "", Description: err.Error()}
-		b, _ := json.Marshal(_output)
-		w.Write(b)
-		return
-	}
-
-	defer func() {
-		if err := statefile.Close(); err != nil {
-			fmt.Println(err)
 		}
-		// remove state file after
-		if err := os.Remove(stateFilePath); err != nil {
-			fmt.Println(err)
-		}
-		return
-	}()
 
-	locustMasterIP, err := terraformOutput(stateFilePath, "locust_master_ip")
-	if err != nil {
-		_output := output{Status: "Failed", IP: "", Auth: "", Description: errors.WithMessage(err, "terraformOutput(locust_master_ip)").Error()}
-		b, _ := json.Marshal(_output)
-		w.Write(b)
-		return
+		if _, err := io.Copy(statefile, result.Body); err != nil {
+			_output := output{Status: "Failed", Grids: nil, Auth: "", Description: err.Error()}
+			b, _ := json.Marshal(_output)
+			w.Write(b)
+			return
+		}
+
+		defer func() {
+			if err := statefile.Close(); err != nil {
+				fmt.Println(err)
+			}
+			// remove state file after
+			if err := os.Remove(stateFilePath); err != nil {
+				fmt.Println(err)
+			}
+			return
+		}()
+
+		locustMasterIP, err := terraformOutput(stateFilePath, "locust_master_ip")
+		if err != nil {
+			_output := output{Status: "Failed", Grids: nil, Auth: "", Description: errors.WithMessage(err, "terraformOutput(locust_master_ip)").Error()}
+			b, _ := json.Marshal(_output)
+			w.Write(b)
+			return
+		}
+		outputGrids = append(outputGrids, outputGrid{IP: locustMasterIP, Name: grid.Name, ID: grid.ID})
 	}
-	_output := output{Status: "Success", IP: locustMasterIP, Auth: cookie.Value, Description: "Call was a success."}
+	_output := output{Status: "Success", Grids: outputGrids, Auth: cookie.Value, Description: "Call was a success."}
 	b, _ := json.Marshal(_output)
 	w.Write(b)
 	return
