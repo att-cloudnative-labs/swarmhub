9ebe80604e4da66f09e25015583cc0a879d40b02
jenglamlow <jenglamlow@gmail.com>
2020-03-20 14:10:24 +0000

    Dev build; update package; strip console.log at prod env

--- services/swarmhub/frontend/vue.config.js
+++ services/swarmhub/frontend/vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-    assetsDir: "static"
+  assetsDir: "static",
+  chainWebpack: (config) => {
+    config.optimization.minimizer('terser').tap((args) => {
+      args[0].terserOptions.compress.drop_console = true
+      return args
+    })
   }
+}
