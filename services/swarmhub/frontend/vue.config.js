ba02d9cddd01ec7230ac029037b793fac024ffdc
jenglamlow <jenglamlow@gmail.com>
2020-03-24 05:01:27 +0000

    enable extract css during development mode

--- services/swarmhub/frontend/vue.config.js
+++ services/swarmhub/frontend/vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   assetsDir: "static",
+  css: {
+    extract: true
+  },
   chainWebpack: (config) => {
     config.optimization.minimizer('terser').tap((args) => {
       args[0].terserOptions.compress.drop_console = true
