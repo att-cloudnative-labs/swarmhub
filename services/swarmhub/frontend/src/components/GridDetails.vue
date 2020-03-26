18cbd86dd554191d452605d0487083bb811f5a85
jenglamlow <jenglamlow@gmail.com>
2020-03-26 12:07:24 +0000

    Mute delete button when grid.status is deleting

--- services/swarmhub/frontend/src/components/GridDetails.vue
+++ services/swarmhub/frontend/src/components/GridDetails.vue
@@ -5,7 +5,7 @@
       <p class="subtitle is-6">
         <button
           class="button is-small is-rounded is-danger"
-          :disabled="grid.Status === 'Deploying' || grid.Status === 'Cleaning' "
+          :disabled="grid.Status === 'Deploying' || grid.Status === 'Cleaning' || grid.Status === 'Deleting'"
           @click="isDeleteGridModalActive = true;"
         >delete</button>
       </p>
